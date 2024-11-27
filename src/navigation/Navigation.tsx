74d83274dd3624724f02c287858e72bd1f15ca93
Fadi1872 <fadinoumih18@gmail.com>
2024-11-27 16:17:31 +0300

    new navigation system

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import { PublicRoutes } from "./routes/PublicRoutes";
+import { AuthRoutes } from "./routes/AuthRoutes";
+import MustNotBeLoggedIn from "./layouts/MustNotBeLoggedIn";
+import NotFound from "../pages/NotFound";
+
+const Navigation = () => {
+  const router = createBrowserRouter([
+    {
+      element: <MainLayout />,
+      children: PublicRoutes,
+    },
+    {
+      element: <MustNotBeLoggedIn />,
+      children: AuthRoutes,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default Navigation;

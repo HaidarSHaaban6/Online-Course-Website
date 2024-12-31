d5a34627918fec3a073be703aea48f7a1f0e0ced
Fadi1872 <fadinoumih18@gmail.com>
2024-12-31 21:30:31 +0300

    sign Up ready - 1

--- src/Services/AuthService.ts
+++ src/Services/AuthService.ts
@@ -1,6 +1,10 @@
 import { User } from "../data/interfaces";
 
 class AuthService {
+  private static isValidEmail(email: string) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
   public static logUserIn(users: User[], loginUser: User) {
     // the errors messages
     let errors: User = {
@@ -10,21 +14,25 @@ class AuthService {
     };
     let errorsExits = false;
     // validating the full name
-    if(loginUser.fullName.length == 0) {
-        errors.fullName = "Full Name Field is required"
-        errorsExits = true;
+    if (loginUser.fullName.length == 0) {
+      errors.fullName = "Full Name Field is required";
+      errorsExits = true;
     }
     // validating the email
     if (loginUser.email.length == 0) {
       errors.email = "Email field is required";
       errorsExits = true;
     } else {
-      const userAreadyExists = users.find(
-        (user) => user.email == loginUser.email
-      );
-      if (userAreadyExists) {
-        errors.email = "Email already in use!";
-        errorsExits = true;
+      if (!this.isValidEmail(loginUser.email)) {
+        errors.email = "This field should be an Email!";
+      } else {
+        const userAreadyExists = users.find(
+          (user) => user.email == loginUser.email
+        );
+        if (userAreadyExists) {
+          errors.email = "Email already in use!";
+          errorsExits = true;
+        }
       }
     }
     // validating the password
@@ -37,7 +45,7 @@ class AuthService {
         errorsExits = true;
       }
     }
-    
+
     return { errors, errorsExits };
   }
 }

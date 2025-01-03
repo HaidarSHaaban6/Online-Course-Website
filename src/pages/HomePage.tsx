fcf5cfdd077d8a53ffabbfdd60c7355042c07b3d
HaidarSHaaban6 <shaabanhaidar569@gmail.com>
2025-01-03 13:52:47 +0300

    Add Some Features

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,24 @@
+import OurCoursesSection from "../components/OurCoursesSection";
+import PageTitle from "../ui/PageTitle";
+import Video from "../ui/Video";
+import VideoIconTag from "../ui/VideoIconTag";
+
+const description =
+  "Welcome to SkillBridge's Pricing Plan page, where we offer two comprehensive options to cater to your needs: Free and Pro. We believe in providing flexible and affordable pricing options for our services. Whether you're an individual looking to enhance your skills or a business seeking professional development solutions, we have a plan that suits you. Explore our pricing options below and choose the one that best fits your requirements.";
 const HomePage = () => {
-  return <div className="bg-white/97"></div>;
+  return (
+    <>
+      <PageTitle
+        title="Online Courses on Design and Development"
+        description={description}
+      />
+      <OurCoursesSection />
+      <div className="custom-container">
+        <Video img="src/assets/img/home_page_frame.png">
+          <VideoIconTag />
+        </Video>
+      </div>
+    </>
+  );
 };
 export default HomePage;
--- src/ui/Video.tsx
+++ src/ui/Video.tsx
@@ -1,3 +1,4 @@
+// Interface Section :
 interface videoProps {
   img: string;
   children: React.ReactNode;
@@ -7,7 +8,7 @@ interface videoProps {
 const Video: React.FC<videoProps> = ({ img, children }) => {
   return (
     <div
-      style={{ backgroundImage: `url(${img})` }}
+      style={{ backgroundImage: `url(./../${img})` }} // Start The Path With src...
       className="flex items-center justify-center bg-center bg-cover rounded-xl video-height"
     >
       <div>{children}</div>
@@ -16,3 +17,9 @@ const Video: React.FC<videoProps> = ({ img, children }) => {
 };
 
 export default Video;
+
+// Example For Use :
+
+// <Video img="src/assets/img/home_page_frame.png">
+//   <VideoIconTag />
+// </Video>;

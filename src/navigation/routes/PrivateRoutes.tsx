6002069c4bb41c760d7e4ba87e26d09f67cf9892
halaalhassanieh <halaalhassanieh9@gmail.com>
2025-01-20 13:12:24 +0300

    Updates

--- src/navigation/routes/PrivateRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { RouteObject } from "react-router-dom";
-import Dashboard from "../../pages/Dashboard";
-
-export const PrivateRoutes: RouteObject[] = [
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-  },
-];
--- src/pages/CoursesOpenPage.tsx
+++
@@ -1,7 +0,0 @@
-import CoursesOpenPageSection from "../components/CourseOpenPageSection";
-
-const CoursesOpenPage = () => {
-  return <CoursesOpenPageSection />;
-};
-
-export default CoursesOpenPage;
